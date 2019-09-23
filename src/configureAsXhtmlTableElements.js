fea569123728e55c50a616ba527a692cb48ad047
Mehmet Coskun <mehmet.coskun@fontoxml.com>
2019-09-23 11:54:48 +0200

    DEV-4371 Add explanation about columnBefore and rowBefore

--- src/configureAsXhtmlTableElements.js
+++ src/configureAsXhtmlTableElements.js
@@ -5,6 +5,9 @@ import XhtmlTableDefinition from './table-definition/XhtmlTableDefinition.js';
 /**
  * Configure XHTML tables.
  *
+ * Note that the `columnBefore` widget area can only be used if your tables use col elements, i.e.,
+ * when `shouldCreateColumnSpecificationNodes` is set to `true`.
+ *
  * Example usage for the table widgets:
  *
  *```
@@ -41,8 +44,8 @@ import XhtmlTableDefinition from './table-definition/XhtmlTableDefinition.js';
  * @param  {number}          [options.priority]                    Selector priority for all elements configured by this function
  * @param  {boolean}         [options.showInsertionWidget]         To add insertion buttons which insert a column or a row to a specific place, default false.
  * @param  {boolean}         [options.showHighlightingWidget]      To add highlighting bars which highlight columns and rows, and provide operations popover, default false.
- * @param  {Widget[]|null}   [options.columnBefore]                To add column icon widgets by using {@link createIconWidget}. Any widget can be added but only icon widget is supported.
- * @param  {Widget[]|null}   [options.rowBefore]                   To add row icon widgets by using {@link createIconWidget}. Any widget can be added but only icon widget is supported.
+ * @param  {Widget[]|null}   [options.columnBefore]                To add column icon widgets by using {@link createIconWidget}. Column widgets are linked to the col elements. Tables that do not have these elements will not show columnBefore widgets. Any widget can be added but only icon widget is supported.
+ * @param  {Widget[]|null}   [options.rowBefore]                   To add row icon widgets by using {@link createIconWidget}. Row widgets are linked to the row elements of the table. Any widget can be added but only icon widget is supported.
  * @param  {boolean}         [options.useTh]                       Set to true if th should be used
  * @param  {boolean}         [options.useThead]                    Set to true if thead should be used
  * @param  {boolean}         [options.useTbody]                    Set to true if tbody should be used
